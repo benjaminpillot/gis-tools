4e377b97cb33c93409f927778d6e4a98ecb71c12
Benjamin2b <benjaminpillot@hotmail.com>
2018-11-05 15:19:07 +0100

    Create __init__.py

---
+++ utils/check/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-7 -*-
+
+""" Module summary description.
+
+More detailed description.
+"""
+
+# __all__ = []
+# __version__ = '0.1'
+__author__ = 'Benjamin Pillot'
+__copyright__ = 'Copyright 2017, Benjamin Pillot'
+__email__ = 'benjaminpillot@riseup.net'
+
+# import
+from utils.check.type import *
+from utils.check.value import *
+from utils.check.descriptor import *
