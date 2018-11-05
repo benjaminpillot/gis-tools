a414d7d8b9f32a69eee21e9d2bfe24bcd9d68825
Benjamin Pillot <benjaminpillot@hotmail.com>
2018-11-05 18:19:19 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='geotools',
+      version='0.1',
+      description='Some geographical tools for Python developers',
+      url='http://github.com/Benjamin2b/geotools',
+      author='Benjamin Pillot',
+      author_email='benjaminpillot@riseup.net',
+      license='GNU GPL v3.0',
+      packages=['geotools'],
+      zip_safe=False)
