aec633ec83a9a907dbd10968746e4f541e91c5b5
Benjamin2b <benjaminpillot@hotmail.com>
2018-11-05 15:19:49 +0100

    Create __init__.py

---
+++ utils/sys/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+""" Module summary description.
+
+More detailed description.
+"""
+
+# __all__ = []
+# __version__ = '0.1'
+__author__ = 'Benjamin Pillot'
+__copyright__ = 'Copyright 2017, Benjamin Pillot'
+__email__ = 'benjaminpillot@riseup.net'
+
+# import
+from utils.sys.browser import *
