3861f49a6a547ad66322647067c81b594dfdec41
Benjamin Pillot <benjaminpillot@hotmail.com>
2018-11-06 02:00:33 +0100

    Create example_1.py

---
+++ examples/zonal_stat/example_1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+""" Example 1
+
+More detailed description.
+"""
+
+# __all__ = []
+# __version__ = '0.1'
+__author__ = 'Benjamin Pillot'
+__copyright__ = 'Copyright 2017, Benjamin Pillot'
+__email__ = 'benjaminpillot@riseup.net'
