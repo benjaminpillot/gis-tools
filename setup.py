e58cfa2fa318ba2b77610c85aabd843dad0e7c5a
Benjamin Pillot <benjaminpillot@hotmail.com>
2018-11-06 13:03:17 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", 'r') as fh:
     long_description = fh.read()
 
 setup(name='gis_tools',
-      version='0.14.2',
+      version='0.14.3',
       description='Some geographical tools for Python developers',
       long_description=long_description,
       long_description_content_type="text/markdown",
@@ -12,5 +12,5 @@ setup(name='gis_tools',
       author='Benjamin Pillot',
       author_email='benjaminpillot@riseup.net',
       license='GNU GPL v3.0',
-      packages=['gis_tools', 'utils', 'toolset'],
+      packages=['gis_tools', 'utils', 'utils.check', 'utils.sys', 'toolset'],
       zip_safe=False)
