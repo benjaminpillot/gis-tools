11e9de5b140d812855ee643515f7a11c7e67e67c
Benjamin Pillot <benjaminpillot@hotmail.com>
2018-11-06 02:21:29 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
+with open("README.md", 'r') as fh:
+    long_description = fh.read()
+
 setup(name='gis_tools',
       version='0.1',
       description='Some geographical tools for Python developers',
