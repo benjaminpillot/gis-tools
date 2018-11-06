f7650c8f4537480dcc678d7933e03e6fd5b8afee
Benjamin Pillot <benjaminpillot@hotmail.com>
2018-11-06 02:21:13 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,11 @@ from setuptools import setup
 setup(name='gis_tools',
       version='0.1',
       description='Some geographical tools for Python developers',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='http://github.com/Benjamin2b/gis_tools',
       author='Benjamin Pillot',
       author_email='benjaminpillot@riseup.net',
       license='GNU GPL v3.0',
-      packages=['gis_tools'],
+      packages=['gis_tools', 'utils'],
       zip_safe=False)
