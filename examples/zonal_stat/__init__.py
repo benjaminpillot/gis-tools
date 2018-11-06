1effd00c174e97e8dc63ee133300f30d0eda992f
Benjamin Pillot <benjaminpillot@hotmail.com>
2018-11-06 01:58:22 +0100

    Create __init__.py

---
+++ examples/zonal_stat/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+""" Examples on how to use ZonalStatistics class
+
+More detailed description.
+"""
+
+# __all__ = []
+# __version__ = '0.1'
+__author__ = 'Benjamin Pillot'
+__copyright__ = 'Copyright 2017, Benjamin Pillot'
+__email__ = 'benjaminpillot@riseup.net'
