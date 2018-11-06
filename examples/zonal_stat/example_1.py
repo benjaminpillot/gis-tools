7901f6c431f84534272d2302e1b8921651a7debc
Benjamin Pillot <benjaminpillot@hotmail.com>
2018-11-06 03:07:40 +0100

    Update example_1.py

--- examples/zonal_stat/example_1.py
+++ examples/zonal_stat/example_1.py
@@ -5,7 +5,7 @@
 More detailed description.
 """
 
-from gis_tools.raster import DigitalElevationModel
+from gis_tools.raster import RasterMap
 from gis_tools.layer import PolygonLayer
 
 # __all__ = []
@@ -16,19 +16,16 @@ __email__ = 'benjaminpillot@riseup.net'
 
 
 # Create a digital elevation model from SRTM DEM of French Guiana
-dem = DigitalElevationModel("srtm_guyana.tif", no_data_value=-32768)
-
-# Convert DEM to UTM coordinate reference system
-dem_utm = dem.to_crs({'init': 'epsg:32622'})
-
-# Compute slope from the DEM
-slope = dem_utm.compute_slope()
+biomass = RasterMap("biomasse_ressource.tif", no_data_value=-9999)
 
 # Import a geographical layer made of 6 large polygons from a shapefile
 layer = PolygonLayer("enp_pn_s_973.shp")
 
-# Create ZonalStatistics instance from slope and layer. 
-zonal_stat = ZonalStatistics(slope, layer, is_surface_weighted=False, all_touched=True)
+# Convert DEM to layer's coordinate reference system
+biomass = biomass.to_crs(layer.crs)
+
+# Create ZonalStatistics instance from biomass and layer. 
+zonal_stat = ZonalStatistics(biomass, layer, is_surface_weighted=False, all_touched=True)
 
 # Compute slope average within each polygon
 avg = zonal_stat.mean()
