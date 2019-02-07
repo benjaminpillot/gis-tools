b734fa02f5483562971ba29c9f2d15cba09356a2
Benjamin Pillot <benjaminpillot@hotmail.com>
2019-02-07 12:59:30 +0100

    Update version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", 'r') as fh:
     long_description = fh.read()
 
 setup(name='gis_tools',
-      version='0.15.31',
+      version='0.15.32',
       description='Some geographical tools for Python developers',
       long_description=long_description,
       long_description_content_type="text/markdown",
