68e4af9c7bac50bd4ce0b078fb572c7f0a814249
Benjamin Pillot <benjaminpillot@hotmail.com>
2019-02-11 11:19:39 +0100

    New version

--- setup.py
+++ setup.py
@@ -1,17 +1,32 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open("README.md", 'r') as fh:
     long_description = fh.read()
 
 setup(name='gis_tools',
-      version='0.15.4',
+      version='0.15.41',
       description='Some geographical tools for Python developers',
       long_description=long_description,
       long_description_content_type="text/markdown",
       url='http://github.com/benjamin2b/gis_tools',
       author='Benjamin Pillot',
       author_email='benjaminpillot@riseup.net',
+      install_requires=["cpc.geogrids>=0.2.3",
+                        "elevation>=1.0.5",
+                        "fiona>=1.7.13",
+                        "gdal>=2.2.4",
+                        "geopandas>=0.3.0",
+                        "matplotlib>=2.2.3",
+                        "metis >=0.2a4",
+                        "networkx>=2.1",
+                        "numpy>=1.14.3",
+                        "pandas >=0.23.4",
+                        "progressbar2>=3.38.0",
+                        "pyproj>=1.9.5",
+                        "rasterio>=0.36.0",
+                        "rtree>=0.8.3",
+                        "scipy>=1.1.0",
+                        "shapely>=1.6.4"],
       license='GNU GPL v3.0',
-      packages=['gistools', 'gistools.utils', 'gistools.utils.check', 'gistools.utils.sys', 'gistools.toolset',
-                'gistools.examples'],
+      packages=find_packages(),
       zip_safe=False)
