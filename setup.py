1e020f090b20cd7371328910e63491077688131c
pillot <benjamin.pillot@ird.fr>
2019-11-13 19:10:16 +0100

    install_requires from requirements.txt

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ import gistools
 with open("README.md", 'r') as fh:
     long_description = fh.read()
 
+with open("requirements.txt") as req:
+    install_req = req.read().splitlines()
+
 setup(name='gis_tools',
       version=gistools.__version__,
       description='Some geographical tools for Python developers',
@@ -13,25 +16,7 @@ setup(name='gis_tools',
       url='http://github.com/benjaminpillot/gis-tools',
       author='Benjamin Pillot',
       author_email='benjaminpillot@riseup.net',
-      install_requires=["cpc.geogrids==0.2.3",
-                        "elevation>=1.0.5",
-                        "fiona>=1.7.13",
-                        "gdal>=2.2.4",
-                        "geopandas>=0.4.0",
-                        "greece-utils>=0.1.0"
-                        "matplotlib>=3.0.3",
-                        "metis>=0.2a4",
-                        "networkx>=2.1",
-                        "numba>=0.44.1",
-                        "numpy>=1.14.3",
-                        "pandas>=0.24.2",
-                        "progressbar2>=3.39.3",
-                        "pyproj==2.0.2",
-                        "rasterio>=1.0.18",
-                        "rdp>=0.8",
-                        "rtree>=0.8.3",
-                        "scipy>=1.1.0",
-                        "shapely>=1.6.4"],
+      install_requires=install_req,
       python_requires='>=3',
       license='GNU GPL v3.0',
       packages=find_packages(),
