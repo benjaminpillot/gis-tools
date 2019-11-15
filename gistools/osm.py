c0566a32d8d4ead8800c46a29f4ef08779547002
pillot <benjamin.pillot@ird.fr>
2019-11-15 19:09:57 +0100

    New module

---
+++ gistools/osm.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+""" OpenStreetMap related tools
+
+More detailed description.
+"""
+
+__author__ = 'Benjamin Pillot'
+__copyright__ = 'Copyright 2019, Benjamin Pillot'
+__email__ = 'benjaminpillot@riseup.net'
