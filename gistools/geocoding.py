03122e092c4c80bf458419551456a47f416a71a3
benjamin <benjamin.pillot@ird.fr>
2019-12-11 00:23:09 +0100

    Update

--- gistools/geocoding.py
+++ gistools/geocoding.py
@@ -24,6 +24,11 @@ def all_possible_addresses(list_of_polygon_layers, address_attribute_name='name'
     :param keeping_attributes: optional attributes to keep in layers (list of str)
     :return:
     """
+    if keeping_attributes is None:
+        keeping_attributes = [address_attribute_name]
+    else:
+        keeping_attributes = [address_attribute_name] + keeping_attributes
+
     if not is_iterable(list_of_polygon_layers):
         raise TypeError("Input must be an iterable but is '%s'" % type(list_of_polygon_layers).__name__)
 
@@ -31,10 +36,11 @@ def all_possible_addresses(list_of_polygon_layers, address_attribute_name='name'
         if not isinstance(layer, PolygonLayer):
             raise TypeError("All layers in iterable must be of type 'PolygonLayer'")
 
+    addresses = []
     intersection_level = 0
     address_levels = ["level%d" % n for n in range(len(list_of_polygon_layers))]
-    layers = [layer.rename(address_attribute_name, "level%d" % n) for n, layer in enumerate(list_of_polygon_layers)]
-    addresses = []
+    layers = [layer.keep_attributes(keeping_attributes).rename(address_attribute_name, "level%d" % n)
+              for n, layer in enumerate(list_of_polygon_layers)]
 
     while "there are deeper levels of intersection":
 
@@ -49,12 +55,6 @@ def all_possible_addresses(list_of_polygon_layers, address_attribute_name='name'
         else:
             intersection_level += 1
 
-    if keeping_attributes:
-        keeping_attributes = ['address', 'min_delta', 'max_delta'].extend(keeping_attributes)
-    else:
-        keeping_attributes = ['address', 'min_delta', 'max_delta']
-
-    addresses = [address.keep_attributes(keeping_attributes) for address in addresses]
     result = addresses[0]
 
     for address in addresses[1::]:
