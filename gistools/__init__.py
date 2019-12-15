0ce3934d7e9e30507066d9b8d139cee15557219b
benjamin <benjamin.pillot@ird.fr>
2019-12-15 23:52:56 +0100

    Add GEOMETRY_CLASS

--- gistools/__init__.py
+++ gistools/__init__.py
@@ -6,6 +6,8 @@ More detailed description.
 """
 
 # __all__ = []
+from shapely.geometry import LineString, MultiLineString, Polygon, MultiPolygon, Point, MultiPoint
+
 __version__ = '0.15.94'
 __author__ = 'Benjamin Pillot'
 __copyright__ = 'Copyright 2019, Benjamin Pillot'
@@ -15,3 +17,7 @@ __email__ = 'benjaminpillot@riseup.net'
 # from gistools.raster import *
 # from gistools.network import *
 # from gistools.stats import *
+
+
+GEOMETRY_CLASS = {'linestring': (LineString, MultiLineString), 'polygon': (Polygon, MultiPolygon),
+                  'point': (Point, MultiPoint)}
