e29c2a8a977c1e76189fb7c55986de996475ea8d
pillot <benjamin.pillot@ird.fr>
2019-12-20 19:03:59 +0100

    Update address parser

--- gistools/geocoding.py
+++ gistools/geocoding.py
@@ -5,6 +5,7 @@
 More detailed description.
 """
 from abc import abstractmethod
+from collections import OrderedDict
 from itertools import combinations
 
 from numpy import unique
@@ -62,10 +63,11 @@ def all_addresses(list_of_polygon_layers, by='name', to='name', keeping_attribut
             intersection_level += 1
 
     result = concat_layers(addresses)
-    result['min_delta'], result['max_delta'] = result.distance_of_centroid_to_boundary()
     result[to] = result["level0"].str.cat([result[level] for level in address_levels[1::]], sep=',', na_rep="")
+    result = result.dissolve(by=to)
+    result['min_delta'], result['max_delta'] = result.distance_of_centroid_to_boundary()
 
-    return result.dissolve(by=to)
+    return result
 
 
 class Address:
@@ -124,14 +126,14 @@ class Address:
     def geocode(self, address_converter):
         """ Geocode addresses using converter model
 
-        :param address_converter: AddressConverter instance
+        :param address_converter: AddressParser instance
         :return:
         """
         pass
 
 
-class AddressConverter:
-    """ Convert address using corresponding model
+class AddressParser:
+    """ Parse address using corresponding model
 
     """
 
@@ -139,88 +141,41 @@ class AddressConverter:
         pass
 
     @abstractmethod
-    def convert(self, *args, **kwargs):
+    def parse(self, *args, **kwargs):
         pass
 
 
-class DictionaryConverter(AddressConverter):
-    """
+class XParser(AddressParser):
+    """ XParser is an address parser gathering linguistic techniques
+     for parsing addresses (regular expressions + edition distance)
 
     """
-    _dictionary = None
-    _dictionary_columns = ('old', 'new', 'level')
+    _re_parser = OrderedDict()
 
-    _address_levels = None
-
-    def __init__(self, dictionary_file):
+    def __init__(self):
         """
 
-        :param dictionary_file:
         """
         super().__init__()
-        self.dictionary = dictionary_file
-        self._address_levels = unique(self.dictionary["level"])
 
-    def convert(self, addresses):
-        """ Convert old address string to new format
-
-        :param addresses: pandas Series
-        :return:
-        """
-        converted_addresses = addresses.copy()
-        for add_level in self._address_levels:
-            level_dic = self.dictionary[self.dictionary["level"] == add_level]
-            for n, address in enumerate(addresses):
-                old_new = level_dic['old'].apply(lambda old: old in address) & \
-                          level_dic['new'].apply(lambda new: new not in address)
-                converter = level_dic[old_new].squeeze()
-
-                if converter.size != 0:
-                    converted_addresses[n] = converted_addresses[n].replace(converter['old'], converter['new'])
-
-        return converted_addresses
-
-        # new_address = address.apply(lambda adr: adr.replace(self.dictionary["old"], self.dictionary["new"]))
-
-    @property
-    def dictionary(self):
-        return self._dictionary
-
-    @dictionary.setter
-    def dictionary(self, dictionary_file):
-        dic = read_csv(dictionary_file, keep_default_na=False)
-        for key in dic.columns:
-            try:
-                check_string(key, self._dictionary_columns)
-            except ValueError:
-                raise DictionaryConverterError("Dictionary must have the 3 following columns: ('old', 'new', 'level')")
-        self._dictionary = dic
-
-
-class LevenshteinConverter(AddressConverter):
-    """
-
-    """
-
-    def convert(self):
+    def parse(self, *args, **kwargs):
         pass
 
 
 if __name__ == "__main__":
     from utils.sys.timer import Timer
     import pandas as pd
-    test = DictionaryConverter("/home/benjamin/Desktop/APUREZA/geocoding/dictionary/dictionary.csv")
-    dengue = pd.read_csv("/home/benjamin/Desktop/APUREZA/geocoding/dengue_database/dengue.csv")
-    addr = dengue["NM_LOGRADO"].str.cat([dengue["NM_COMPLEM"], dengue["NM_REFEREN"]], sep=',', na_rep="")
-    with Timer() as t:
-        new_addr = test.convert(addr)
-    print("spent time: %s" % t)
+    # test = XParser("/home/benjamin/Desktop/APUREZA/geocoding/dictionary/dictionary.csv")
+    # dengue = pd.read_csv("/home/benjamin/Desktop/APUREZA/geocoding/dengue_database/dengue.csv")
+    # addr = dengue["NM_LOGRADO"].str.cat([dengue["NM_COMPLEM"], dengue["NM_REFEREN"]], sep=',', na_rep="")
+    # with Timer() as t:
+    #     new_addr = test.parse(addr)
+    # print("spent time: %s" % t)
 
-    # from gistools.layer import LineLayer
-    # test = Address("Sao Sebastiao, Distrito Federal")
-    # test.get_osm_layers([("admin_level", ("10", "11")), ("place", "quarter"), ("place", "city_block"),
-    #                      ("highway",)], crs=32723).all_addresses()
-    # test.addresses.to_file("/home/benjamin/Desktop/APUREZA/geocoding/addresses.shp")
+    test = Address("Sao Sebastiao, Distrito Federal")
+    test.get_osm_layers([("admin_level", ("10", "11")), ("place", "quarter"), ("place", "city_block"),
+                         ("highway",)], crs=32723).all_addresses()
+    test.addresses.to_file("/home/benjamin/Desktop/APUREZA/geocoding/addresses.shp")
 
 
     # admin_l10_l11 = PolygonLayer.from_osm("Sao Sebastiao, Distrito Federal", 'admin_level', ("10", "11")).to_crs(
