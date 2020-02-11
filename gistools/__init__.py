99b772af56027a5fccd1e6cf2019bd4a49f5ee5a
pillot <benjamin.pillot@ird.fr>
2020-02-11 17:59:02 +0100

    Add documentation

--- gistools/__init__.py
+++ gistools/__init__.py
@@ -1,8 +1,27 @@
 # -*- coding: utf-8 -*-
 
-""" Module summary description.
+""" A set of geographical tools for Python developers.
 
-More detailed description.
+GisTools is a small Python library for performing
+geographical computations. Typically, it gathers
+different tools from well-known libraries such as
+gdal, rasterio, geopandas, fiona and shapely.
+It allows easily mixing operations between vectors
+and raster maps (multi-band raster are not supported
+at the moment).
+
+GisTools allows some of the following operations:
+- Fast polygon intersection and split
+- Polygon partition based on graph theory
+(requires [METIS](http://glaros.dtc.umn.edu/gkhome/metis/metis/download) package)
+- Basic networking (shortest path)
+- Download DEM from online databases
+- Download OSM layers through Overpass API
+- Download layers from postgis spatial database
+- Extract raster statistics with respect to vector layers (polygon/line)
+- Raster to/from polygons conversion
+- Compute horizon obstruction from DEM
+
 """
 
 # __all__ = []
@@ -12,6 +31,7 @@ __version__ = '0.16.01'
 __author__ = 'Benjamin Pillot'
 __copyright__ = 'Copyright 2019, Benjamin Pillot'
 __email__ = 'benjaminpillot@riseup.net'
+__licence__ = "GNU GPL v3.0"
 
 # from gistools.layer import *
 # from gistools.raster import *
